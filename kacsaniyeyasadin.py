bcbb2c4848cabb21b87cf3c635e58756fb438992
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-04-21 09:10:32 -0400

    Update kacsaniyeyasadin.py

--- kacsaniyeyasadin.py
+++ kacsaniyeyasadin.py
@@ -3,6 +3,21 @@
 
 from datetime import *
 
+kacsaniyeyasadin_ico = """
+#########################################################
+#         KAÇ SANİYE YAŞADIN ? - GH0ST S0FTWARE         #
+######################################################### 
+#                       CONTACT                         #
+#########################################################
+#              DEVELOPER : İSMAİL TAŞDELEN              #                       
+#        Mail Address : pentestdatabase@gmail.com       #
+# LINKEDIN : https://www.linkedin.com/in/ismailtasdelen #
+#           Whatsapp : + 90 534 295 94 31               #
+#########################################################
+"""
+
+print kacsaniyeyasadin_ico
+
 dogum_tarihi = datetime.strptime(raw_input("Doğum gününüz (gün.ay.yıl): "), "%d.%m.%Y")
 
 yas = datetime.now() - dogum_tarihi
